5d967b60afafe3f99623ce8a8cb7f5133229eae6
Michael Ballard <bookstore707@gmail.com>
2020-08-19 21:57:29 -0400

    Updates

---
+++ precache-manifest.3f2a03f0b8b3bb891806d43f6e4ac90a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e874734386fc691ae718f5665aaa6c27",
+    "url": "/YoutubeMiniClone/index.html"
+  },
+  {
+    "revision": "85b37c5636bd217f5bf6",
+    "url": "/YoutubeMiniClone/static/css/main.a6534655.chunk.css"
+  },
+  {
+    "revision": "c9d2ef087a39bc152cee",
+    "url": "/YoutubeMiniClone/static/js/2.c3d3199f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/YoutubeMiniClone/static/js/2.c3d3199f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85b37c5636bd217f5bf6",
+    "url": "/YoutubeMiniClone/static/js/main.b3e158be.chunk.js"
+  },
+  {
+    "revision": "f3ad46d5675c9a2f6f8b",
+    "url": "/YoutubeMiniClone/static/js/runtime-main.63147dca.js"
+  }
+]);
